ebfbd5b1f70fd32b3eddcf521f63328caeea73ee
Vinícius Barros <vmbarross@gmail.com>
2020-09-20 20:13:22 +0100

    Terraform to deploy to Azure

---
+++ terraform/azure/main.tf
@@ -0,0 +1,36 @@
+
+provider "azurerm" {
+  subscription_id = var.subscription
+  tenant_id       = var.tenant_id
+  features {}
+  version = "2.28.0"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group
+  location = var.location
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "msc-container"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "public"
+  os_type             = "linux"
+  dns_name_label      = var.dns
+
+  container {
+    name   = "hw"
+    image  = var.docker_image
+    cpu    = var.cpu
+    memory = var.memory
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
---
+++ terraform/azure/variables.tf
@@ -0,0 +1,39 @@
+variable "subscription" {
+  description = "Azure Subscription ID"
+  default     = ""
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  default     = ""
+}
+
+variable "resource_group" {
+  description = "Azure Tenant ID"
+  default     = "msc-caas-resource-group"
+}
+
+variable "dns" {
+  description = "The dns to be created"
+  default     = "msc-caas"
+}
+
+variable "location" {
+  description = "The Azure location where all resources in this example should be created"
+  default     = "North Europe"
+}
+
+variable docker_image {
+    description = "Docker image to be deployed"
+    default = "viniciusbarros/msc-performance-web-app:latest"
+}
+
+variable cpu {
+    description = "Amount of CPU to be used"
+    default = "1"
+}
+
+variable memory  {
+    description = "Amount of RAM to be used"
+    default = "0.5"
+}
