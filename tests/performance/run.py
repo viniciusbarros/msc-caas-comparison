bc9dba79b92b4d85a891c466bfb4089027726f4e
Vinícius Barros <vmbarross@gmail.com>
2020-09-30 21:18:51 +0100

    Running performance tests in AWS as well

--- tests/performance/run.py
+++ tests/performance/run.py
@@ -23,38 +23,38 @@ class Runner:
         self.cases = [
             "k6 run -e DOMAIN='{domain}' -e ENDPOINT='' hello-world.js --insecure-skip-tls-verify --summary-export=metrics/{now}_hello_world_{hosting}_{cpu}_cpu_{memory}_memory.json",
             # 0.00001
-            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='/cpu/factorial/10' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_10_{hosting}_{cpu}_cpu_{memory}_memory.json",
+            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='cpu/factorial/10' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_10_{hosting}_{cpu}_cpu_{memory}_memory.json",
             # 0.0005
-            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='/cpu/factorial/1000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_1000_{hosting}_{cpu}_cpu_{memory}_memory.json",
+            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='cpu/factorial/1000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_1000_{hosting}_{cpu}_cpu_{memory}_memory.json",
             # 0.05
-            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='/cpu/factorial/10000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_10000_{hosting}_{cpu}_cpu_{memory}_memory.json",
+            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='cpu/factorial/10000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_10000_{hosting}_{cpu}_cpu_{memory}_memory.json",
             # 0.5
-            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='/cpu/factorial/32000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_32000_{hosting}_{cpu}_cpu_{memory}_memory.json",
+            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='cpu/factorial/32000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_32000_{hosting}_{cpu}_cpu_{memory}_memory.json",
             # 1s
-            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='/cpu/factorial/43000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_43000_{hosting}_{cpu}_cpu_{memory}_memory.json",
+            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='cpu/factorial/43000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_43000_{hosting}_{cpu}_cpu_{memory}_memory.json",
             # 2s
-            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='/cpu/factorial/50000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_50000_{hosting}_{cpu}_cpu_{memory}_memory.json",
+            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='cpu/factorial/50000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_50000_{hosting}_{cpu}_cpu_{memory}_memory.json",
             # 3s
-            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='/cpu/factorial/60000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_60000_{hosting}_{cpu}_cpu_{memory}_memory.json",
+            "k6 run -e DOMAIN='{domain}' -e ENDPOINT='cpu/factorial/60000' cpu.js --insecure-skip-tls-verify --summary-export=metrics/{now}_factorial_60000_{hosting}_{cpu}_cpu_{memory}_memory.json",
         ]
 
         self.scenarios = [
-            {
-                "hosting": "docker",
-                "configs": [
-                    {
-                        "cpu": '1',
-                        "memory": '2'
-                    },
-                    {
-                        "cpu": '2',
-                        "memory": '2'
-                    }
-                ],
-                "start": "docker run --rm -d -p 8080:80 --name=caas-local --cpus={cpu} --memory={memory}g msc-performance-web-app:latest",
-                "stop": "docker stop caas-local && docker rm caas-local",
-                "domain": "http://localhost:8080"
-            },
+            # {
+            #     "hosting": "docker",
+            #     "configs": [
+            #         {
+            #             "cpu": '1',
+            #             "memory": '2'
+            #         },
+            #         {
+            #             "cpu": '2',
+            #             "memory": '2'
+            #         }
+            #     ],
+            #     "start": "docker run --rm -d -p 8080:80 --name=caas-local --cpus={cpu} --memory={memory}g msc-performance-web-app:latest",
+            #     "stop": "docker stop caas-local && docker rm caas-local",
+            #     "domain": "http://localhost:8080"
+            # },
             {
                 "hosting": "gcp",
                 "configs": [
@@ -70,6 +70,22 @@ class Runner:
                 "start": "cd ../../terraform/gcp && terraform plan -out plan -var 'cpu={cpu}' -var 'memory={memory}' && terraform apply -auto-approve",
                 "stop": "cd ../../terraform/gcp && terraform destroy -auto-approve",
                 "get_url": 'cd ../../terraform/gcp && terraform output -json service_url'
+            },
+            {
+                "hosting": "aws",
+                "configs": [
+                    {
+                        "cpu": '1024',
+                        "memory": '2048'
+                    },
+                    {
+                        "cpu": '2048',
+                        "memory": '2048'
+                    }
+                ],
+                "start": "cd ../../terraform/aws && terraform plan -out plan -var 'cpu={cpu}' -var 'memory={memory}' && terraform apply -auto-approve",
+                "stop": "cd ../../terraform/aws && terraform destroy -auto-approve",
+                "get_networks": 'aws ec2 describe-network-interfaces'
             }
         ]
 
@@ -84,6 +100,8 @@ class Runner:
                 self.run_docker(scenario)
             elif hosting == 'gcp':
                 self.run_gcp(scenario)
+            elif hosting == 'aws':
+                self.run_aws(scenario)
             else:
                 logging.warning(f'Hosting {hosting} is not yet supported')
         logging.info("--------Ending Script Runner--------")
@@ -157,6 +175,43 @@ class Runner:
 
             # Stopping Container
             logging.info(f"Destroying GCP Cloud Run w/ CPU: {cpu} and Memory: {memory}")
+            self.run_command(scenario.get('stop'))
+
+    def run_aws(self, scenario):
+        logging.info(f"Let's run this AWS ECS Fargate scenario.")
+        logging.info(f"{len(scenario.get('configs',[]))} config(s) to run!")
+
+        for conf in scenario.get('configs', []):
+            cpu = conf.get('cpu')
+            memory = conf.get('memory')
+
+            start_cmd = scenario.get('start').format(
+                cpu=cpu,
+                memory=memory
+            )
+
+            # Provisioning infrastructure
+            logging.info(
+                f"Creating AWS ECS Fargate w/ CPU: {cpu} and Memory: {memory}")
+            self.run_command(start_cmd)
+
+            # Getting instance IP
+            # Note: Unfortunatelly  ECS/Terraform dont provide an easy way to get the IP or domain
+            logging.info('Sleeping to give AWS enough time to register the network interfaces')
+            time.sleep(30)
+            networks = json.loads(self.run_command(scenario.get('get_networks')))
+            url = 'http://' + networks['NetworkInterfaces'][0]['Association']['PublicDnsName']
+
+            # Running Test Cases
+            for case in self.cases:
+                run_cmd = case.format(cpu=cpu, memory=memory, hosting=scenario.get(
+                    'hosting'), domain=url, now=datetime.now().strftime("%Y_%m_%d_%H%M%S"))
+                self.run_command(run_cmd)
+                # Sleeping between
+                time.sleep(self.sleep_time_between_cases)
+
+            # Stopping Container
+            logging.info(f"Destroying AWS ECS Fargate w/ CPU: {cpu} and Memory: {memory}")
             self.run_command(scenario.get('stop'))
 
 
